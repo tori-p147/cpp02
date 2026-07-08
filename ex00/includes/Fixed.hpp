6336cdfcc476fd603931143afb9ad671ab146e22
vmatsuda <majtyan@gmail.com>
2026-07-09 06:33:00 +0900

    ex00 done

--- ex00/includes/Fixed.hpp
+++ ex00/includes/Fixed.hpp
@@ -1,14 +1,31 @@
-#ifndef FIXED_H
-#define FIXED_H
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
-#include <iostream>
+# include <iostream>
+# include <string>
 
-class Fixed {
-
+class Fixed
+{
 	public:
-
+		// Constructors
+		Fixed();
+		Fixed(const Fixed &copy);
+		Fixed(int rawBits);
+		
+		// Destructor
+		~Fixed();
+		
+		// Assignment operator
+		Fixed& operator=(const Fixed& assign);
+		
+		// Getters / Setters
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		
 	private:
-
+		int _rawBits;
+		static const int _fracBits = 8;
+		
 };
 
 #endif
--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -1,1 +1,47 @@
-#include <Fixed.hpp>
+#include "Fixed.hpp"
+
+// Constructors
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	_rawBits = 0;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_rawBits = copy.getRawBits();
+}
+
+Fixed::Fixed(int rawBits)
+{
+	std::cout << "Fields constructor called" << std::endl;
+	_rawBits = rawBits;
+}
+
+// Destructor
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// Assignment operator
+Fixed & Fixed::operator=(const Fixed &assign)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_rawBits = assign.getRawBits();
+	return *this;
+}
+
+// Getter
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _rawBits;
+}
+
+// Setter
+void Fixed::setRawBits(int const raw)
+{
+	_rawBits = raw;
+}
